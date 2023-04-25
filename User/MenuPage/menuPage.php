318d31d3ce5e13d834e760bc72bd10b73903c930
appu-kuria <appu.kurian.ak@gmail.com>
2023-04-25 14:13:02 +0530

    Rewamped the menu page

--- User/MenuPage/menuPage.php
+++ User/MenuPage/menuPage.php
@@ -1,8 +1,8 @@
 <html>
 
 <head>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="./menuPage.css">
 </head>
 
@@ -28,6 +28,8 @@
                 <div class="category_content">
                     <h1>Outlet Name</h1>
                     <h2>Outlet Address, Sample PO, Sample Place</h2>
+                    <a href="./menusample.php"> Click here to go to Menu Sample Page</a>
+                    <a href="./test.php"> Click here to go to Menu test Page</a>
                 </div>
             </div>
             <div class="display_flex justify_end width_30">
